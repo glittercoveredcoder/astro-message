aceaf6e96454ee6deac69384a0e00107757e9b9b
glittercoveredcoder <alyssalrd@gmail.com>
2020-11-04 10:51:19 -0700

    wrote messages as arrays in an object

---
+++ main.js
@@ -0,0 +1,41 @@
+const astroMessages = {
+  Aries: [
+    "It's a good time to begin a new project you've been putting off -- start the plan",
+    ""
+  ],
+  Taurus: [
+    "Like most days, today is a good one to spend on yourself. Indulge in your favorites."
+  ],
+  Gemini: [
+    "Too much focus on partners could eventually leave you feeling neglected. Balance it."
+  ],
+  Cancer: [
+    "The hard choices you're making today are going to push your life in a new direction."
+  ],
+  Leo: [
+    "A friendship is entering a new phase, so communication may be strained for a while."
+  ],
+  Virgo: [
+    "Talking about what you stand for and truly believe is a good way to spend this day."
+  ],
+  Libra: [
+    "Take advantage of your high energy to communicate complicated goals and ideas."
+  ],
+  Scorpio: [
+    "Sometimes it's not about winning or losing. Giving in altogether may not be so bad."
+  ],
+  Sagittarius: [
+    "You're especially tuned in to your group of friends now. Get ready to have some fun!"
+  ],
+  Capricorn: [
+    "Go ahead and take the initiative on a work project or two. Your boss will be happy!"
+  ],
+  Aquarius: [
+    "Investigate a different culture or philosophy to find the comfort you need now."
+  ],
+  Pisces: [
+    "Finding a balance between business and pleasure means you'll have to reorganize."
+  ]
+};
+
+console.log(astroMessages.Aquarius);
